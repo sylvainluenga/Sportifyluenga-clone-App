13ae492037673ecb28bfd330870089ae568a0d6f
Alex Sylvain Luenga <38809975+sylvainluenga@users.noreply.github.com>
2024-01-10 11:57:38 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react"
 import './App.css';
-
+import Login from "./Login";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello I will be building a sportifyluenga-clone -AP</h1>
-    </div>
-       
+    // BEM
+    <div className="app">
+      <h1>Hello I will be building a spotifyluenga-clone -AP</h1>
+
+      {/* Spotify Logo */}
+      {/* Login with spotify button */}
+      <Login />
+    </div>    
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="login">
+        <h1>I am the Login Page </h1>
+      {/* Spotify logo */}
+      {/* Login with Spotify button */}
+
+    </div>
+  )
+}
+
+export default Login
