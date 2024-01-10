f672d8f0d9d0ee1396f3edf1a33d03276063fdd4
Alex Sylvain Luenga <38809975+sylvainluenga@users.noreply.github.com>
2024-01-10 12:14:10 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -5,12 +5,8 @@ function App() {
   return (
     // BEM
     <div className="app">
-      <h1>Hello I will be building a spotifyluenga-clone -AP</h1>
-
-      {/* Spotify Logo */}
-      {/* Login with spotify button */}
       <Login />
-    </div>    
+    </div>      
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -4,9 +4,11 @@ import './Login.css';
 function Login() {
   return (
     <div className="login">
-        <h1>I am the Login Page </h1>
       {/* Spotify logo */}
+      <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg "   alt=""/>
+      
       {/* Login with Spotify button */}
+      <button>LOGIN WITH SPOTIFY</button>
 
     </div>
   )
