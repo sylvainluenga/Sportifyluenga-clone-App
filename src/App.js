0a0c671a78d9900ba5659399058864b8cd43ef6c
Alex Sylvain Luenga <38809975+sylvainluenga@users.noreply.github.com>
2024-01-11 13:03:38 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+
 function App() {
+  const [token, setToken] = useState(null);
+
+  //this effect runs the code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log("I HAVE A TOKEN", token);
+ }, []);
+
   return (
-    // BEM stopped at40 min
+    // stopped at
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <h1> I am logged in </h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>      
   );
 }
